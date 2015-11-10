bb412ef3a4fbf4b74d33cdf7d7391bc00b3c1073
Ankit <ankit.sharma@utexas.edu>
2015-11-09 20:22:00 -0600

    bugs have arrived!

--- src/project5/CritterView.java
+++ src/project5/CritterView.java
@@ -75,7 +75,7 @@ public class CritterView extends Application {
 	        VBox vbox = addVBox();
 	        border.setLeft(vbox);
 	        
-	        canvas = new Canvas(600, 500);
+	        canvas = new Canvas(Params.world_height, Params.world_width);
 	        border.setCenter(canvas);
 	        globalStage.setScene(new Scene(border, 300, 250));
 	        globalStage.show();
@@ -169,55 +169,34 @@ public class CritterView extends Application {
 	    }
 	    	
 	    private void drawShapes(GraphicsContext gc) {
-
-	        gc.setFill(Color.GREEN);
-	        gc.setStroke(Color.BLUE);
-	        gc.setLineWidth(5);
-	        gc.strokeLine(40, 10, 10, 40);
-	        gc.fillOval(10, 60, 30, 30);
-	        gc.strokeOval(60, 60, 30, 30);
-	        gc.setLineWidth(1);
-	        gc.strokeOval(150, 60, 10, 10);
-	        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-	        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-	        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-	        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-	        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-	        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-	        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-	        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-	        gc.fillPolygon(new double[]{10, 40, 10, 40},
-	                       new double[]{210, 210, 240, 240}, 4);
 	        
 	    	if(CritterWorld.critterList == null) return;
 	    	
 	    	
 	    	
-//	    	for(Critter c : CritterWorld.critterList) {
-//	    		
-//	    		int x = c.x_coord;
-//	    		int y = c.y_coord;
-//	    		
-//	    		GraphicsContext gc = CritterView.canvas;
-//	    		
-//	    		gc.setFill(c.viewColor());
-//	    		
-//	    		switch(c.viewShape()) {
-//	    		case SQUARE:
-//	    			gc.strokeRect((double) x, (double) y, 3, 3);
-//	    			break;
-//	    		case CIRCLE:
-//	    			gc.strokeOval((double) x, (double) y, 3, 3);
-//	    			break;
-//	    		case DIAMOND:
-//	    		case TRIANGLE:
-//	    		
-//	    		default:
-//	    			break;
-//	    		}
-//	    		
-//	    		
-//	    	}
+	    	for(Critter c : CritterWorld.critterList) {
+	    		
+	    		int x = c.x_coord;
+	    		int y = c.y_coord;
+	    		
+	    		gc.setFill(c.viewColor());
+	    		
+	    		switch(c.viewShape()) {
+	    		case SQUARE:
+	    			gc.strokeRect((double) x, (double) y, 3, 3);
+	    			break;
+	    		case CIRCLE:
+	    			gc.strokeOval((double) x, (double) y, 3, 3);
+	    			break;
+	    		case DIAMOND:
+	    		case TRIANGLE:
+	    		
+	    		default:
+	    			break;
+	    		}
+	    		
+	    		
+	    	}
 	    	
 	    }
 	        
